a204c571828ef110a9e0dedd73a79c84265f85c2
DESKTOP-1DR6FF4\Anas.O <anas.o@aigate.me>
2024-02-19 16:52:36 +0300

    empty-history

--- screens/user-history-page.tsx
+++ screens/user-history-page.tsx
@@ -1,8 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import * as React from "react";
-import { StyleSheet, ActivityIndicator, FlatList, View, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+  View,
+  ScrollView,
+  Text,
+  Image,
+} from "react-native";
 import { fetchUserServices } from "../services/service";
 import UserServiceCard from "../components/user-service-card";
+import { Button } from "react-native-paper";
 
 interface Props {
   route: any;
@@ -19,14 +28,32 @@ const UserHistoryPage: React.FC<Props> = ({ route, navigation }) => {
     return <ActivityIndicator size="large" style={styles.activityIndicator} />;
   }
   return (
-    <View>
-      <ScrollView showsHorizontalScrollIndicator={false}>
-        <>
-          {data?.map((service) => (
-            <UserServiceCard service={service} key={service.id} />
-          ))}
-        </>
-      </ScrollView>
+    <View style={{ flex: 1 }}>
+      {data && data?.length > 0 ? (
+        <ScrollView showsHorizontalScrollIndicator={false}>
+          <>
+            {data?.map((service) => (
+              <UserServiceCard service={service} key={service.id} />
+            ))}
+          </>
+        </ScrollView>
+      ) : (
+        <View style={styles.emptyContainer}>
+          <Image
+            source={require("../assets/support-team-1.png")}
+            style={styles.image}
+          />
+          <Text style={styles.emptyText}>You Have No Previous Services</Text>
+          <Button
+            mode="outlined"
+            onPress={() => {
+              navigation.navigate("categories", { name: "Categories" });
+            }}
+          >
+            Start Ordering
+          </Button>
+        </View>
+      )}
     </View>
   );
 };
@@ -36,6 +63,23 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 100,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+    marginBottom: 20,
+  },
+  emptyText: {
+    fontSize: 18,
+    color: "#555",
+    marginBottom: 20,
   },
 });
 
